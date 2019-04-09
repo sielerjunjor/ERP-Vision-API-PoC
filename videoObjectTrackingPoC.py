5527d70e395f043571221988e26f5056f48d072c
sielerjunjor <sielerjunjor@gmail.com>
2019-04-09 09:41:40 +0200

    added mulitObject Output

--- videoObjectTrackingPoC.py
+++ videoObjectTrackingPoC.py
@@ -20,29 +20,26 @@ print('\nFinished processing.\n')
 # The first result is retrieved because a single video was processed.
 object_annotations = result.annotation_results[0].object_annotations
 
-# Get only the first annotation for demo purposes.
-object_annotation = object_annotations[0]
-print('Entity description: {}'.format(
+#iterate throug all elemets and show their attributes
+for object_annotation in object_annotations:
+    print('Entity description: {}'.format(
     object_annotation.entity.description))
-if object_annotation.entity.entity_id:
-    print('Entity id: {}'.format(object_annotation.entity.entity_id))
-
-print('Segment: {}s to {}s'.format(
+    if object_annotation.entity.entity_id:
+        print('Entity id: {}'.format(object_annotation.entity.entity_id))
+    print('Segment: {}s to {}s'.format(
     object_annotation.segment.start_time_offset.seconds +
     object_annotation.segment.start_time_offset.nanos / 1e9,
     object_annotation.segment.end_time_offset.seconds +
     object_annotation.segment.end_time_offset.nanos / 1e9))
-
-print('Confidence: {}'.format(object_annotation.confidence))
-
-# Here we print only the bounding box of the first frame in this segment
-frame = object_annotation.frames[0]
-box = frame.normalized_bounding_box
-print('Time offset of the first frame: {}s'.format(
-    frame.time_offset.seconds + frame.time_offset.nanos / 1e9))
-print('Bounding box position:')
-print('\tleft  : {}'.format(box.left))
-print('\ttop   : {}'.format(box.top))
-print('\tright : {}'.format(box.right))
-print('\tbottom: {}'.format(box.bottom))
-print('\n')
+    print('Confidence: {}'.format(object_annotation.confidence))
+    # Here we print only the bounding box of the first frame in this segment
+    frame = object_annotation.frames[0]
+    box = frame.normalized_bounding_box
+    print('Time offset of the first frame: {}s'.format(
+        frame.time_offset.seconds + frame.time_offset.nanos / 1e9))
+    print('Bounding box position:')
+    print('\tleft  : {}'.format(box.left))
+    print('\ttop   : {}'.format(box.top))
+    print('\tright : {}'.format(box.right))
+    print('\tbottom: {}'.format(box.bottom))
+    print('\n')
