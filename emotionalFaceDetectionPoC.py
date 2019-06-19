bb75c7d76572f6f8a9a31c799cabe445d07ae3c2
sielerjunjor <sielerjunjor@gmail.com>
2019-06-19 17:12:46 +0200

    addede 2 files

--- emotionalFaceDetectionPoC.py
+++ emotionalFaceDetectionPoC.py
@@ -7,7 +7,7 @@ likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
   'LIKELY', 'VERY_LIKELY')
 for pic in ('face_surprise.jpg', 'face_no_surprise.png'):
   image.source.image_uri = 'gs://cloud-vision-codelab/'+pic
-  resp = client.face_detection(image=image)
+    resp = client.face_detection(image=image)
   faces = resp.face_annotations
   for face in faces:
     print(pic + ': surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
--- landmarkPoC.py
+++ landmarkPoC.py
@@ -3,6 +3,7 @@ from google.cloud.vision import types
 
 client = vision.ImageAnnotatorClient()
 image = vision.types.Image()
-image.source.image_uri = 'gs://cloud-vision-codelab/eiffel_tower.jpg'
+#image.source.image_uri = 'gs://cloud-vision-codelab/eiffel_tower.jpg'
+image.source.image_uri = 'https://www.leipzig.de/fileadmin/_processed_/0/c/csm_Voelkerschlachtdenkmal-blauer-Himmel_LTM_andreas_schmidt-themenbild_eee72a3423.jpg'
 resp = client.landmark_detection(image=image)
 print(resp.landmark_annotations)
